143f0289d0cad3e6871c6d7eae8cb42329f9ac5d
junzhang27 <junzhang27@iflytek.com>
2020-10-27 14:55:22 +0800

    first commit

---
+++ src/main/java/com/ablezha/demo/config/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.ablezha.demo.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootConfiguration
+@ComponentScan("com.ablezha.demo.controller")
+public class ApplicationConfig {
+}
